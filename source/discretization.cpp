e0572f9b02edf44c5d556076f8b83931282c660c
WRLNH <wuyutong030111@gmail.com>
2024-04-28 20:52:33 +0800

    Update discretization

--- source/discretization.cpp
+++ source/discretization.cpp
@@ -1,16 +1,41 @@
 #include "..\include\discretization.h"
 
-double FO_fwrd_diff(int i, double *u, double dx)
+double FO_fwrd_diff(int i, double *u, double dx, int arr_len)
 {
-    return (*(u + i + 1) - *(u + i)) / dx;
+    if (i == arr_len - 1)
+    {
+        return (-(*(u + i - 2)) + 4 * (*(u + i - 1)) - 3 * (*(u + i))) / (2 * dx);
+    }
+    else
+    {
+        return (*(u + i + 1) - *(u + i)) / dx;
+    }
 }
 
-double FO_bwrd_diff(int i, double *u, double dx)
+double FO_bwrd_diff(int i, double *u, double dx, int arr_len)
 {
-    return (*(u + i) - *(u + i - 1)) / dx;
+    if (i == 0)
+    {
+        return (-3 * (*(u + i)) + 4 * (*(u + i + 1)) - (*(u + i + 2))) / (2 * dx);
+    }
+    else
+    {
+        return (*(u + i) - *(u + i - 1)) / dx;
+    }
 }
 
-double SO_ctrl_diff(int i, double *u, double dx)
+double SO_ctrl_diff(int i, double *u, double dx, int arr_len)
 {
-    return (*(u + i + 1) - *(u + i - 1)) / (2 * dx);
+    if (i == 0)
+    {
+        return (-3 * (*(u + i)) + 4 * (*(u + i + 1)) - (*(u + i + 2))) / (2 * dx);
+    }
+    else if (i == arr_len - 1)
+    {
+        return (-(*(u + i - 2)) + 4 * (*(u + i - 1)) - 3 * (*(u + i))) / (2 * dx);
+    }
+    else
+    {
+        return (*(u + i + 1) - *(u + i - 1)) / (2 * dx);
+    }
 }
